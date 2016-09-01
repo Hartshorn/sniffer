54b07cbfcf4352ec34919ab64cb1a61671710d32
michael <mpalmer1661@gmail.com>
2016-09-01 14:52:36 -0500

    additions

--- main.c
+++ main.c
@@ -5,7 +5,58 @@
 int main(void)
 {
 
-		
+
+	FILE *logfile;
+	int tcp = 0;
+	int udp = 0;
+	int icmp = 0;
+	int others = 0;
+	int igmp = 0;
+	int total = 0;
+
+	int i, j;
+
+	struct sockaddr_in source, dest;
+	char hex[2];
+
+	struct ipv4_header *ip_hdr;
+	struct tcp_header  *tcp_hdr;
+	struct udp_header  *udp_hdr;
+	struct icmp_header *icmp_hdr;
+
+	SOCKET sniffer;
+	struct in_addr addr;
+	int in;
+
+	char hostname[100];
+	struct hostent *local;
+	WSADATA wsa;
+
+	if ((logfile = fopen("sniffer.log", "w")) == NULL)
+		printf("unable to create file");
+
+	if (WSAStartup(MAKEWORD(3,2), &wsa) != 0) {
+
+		printf("WSAStartup() failed.\n");
+		return 1;
+	}
+
+	if ((sniffer = socket(AF_INET, SOCK_RAW, IPPROTO_IP)) == INVALID_SOCKET) {
+		printf("Failed to create raw socket.\n");
+		return 1;
+	}
+
+
+
+
+
+
+
+
+
+
+	
+	closesocket(sniffer);
 	
 	return 0;
 
--- sniffer.h
+++ sniffer.h
@@ -27,38 +27,73 @@ void print_data(unsigned char*, int);
 void convert_to_hex(char*, unsigned int);
 
 /* the :4 defines the bit field width */
-typedef struct ip_hdr
+struct ip_header
 {
-	unsigned char ip_header_len: 4;
-	unsigned char ip_version: 4;
-	unsigned char ip_tos; /* ip type of service */
-	unsigned short ip_total_length;
-	unsigned short ip_id;
+	unsigned char ip_version:	4;	/* version: 4 bits */
+	unsigned char ip_header_len:	4;	/* header length: 4 bits */
+	unsigned char ip_tos;			/* ip type of service: 8 bits */
+	unsigned short ip_total_length;		/* total length: 16 bits */
+	unsigned short ip_id;			/* identification: 16 bits */
 
-	unsigned char ip_frag_offset: 5; /* fragment offset field */
-	unsigned char ip_more_fragment: 1;
-	unsigned char ip_dont_fragment: 1;
-	unsigned char ip_reserved_zero: 1;
+	unsigned char ip_frag_offset:	5;	/* fragment offset field: 5 bits */
 
-	unsigned char ip_frag_offset1; /* fragement offset */
+	unsigned char ip_more_fragment: 1;	/* parts of */
+	unsigned char ip_dont_fragment: 1;	/* the fragment */
+	unsigned char ip_reserved_zero: 1;	/* field */
 
-	unsigned char ip_ttl; /* time to live */
-	unsigned char ip_protocol; /* TCP UDP - Protocol field */
-	unsigned short ip_checksum;
-	unsigned int ip_srcaddr;
-	unsigned int ip_destaddr;
+	unsigned char ip_frag_offset1;		/* fragement offset */
 
-} IPV4_HDR;
+	unsigned char ip_ttl;			/* time to live: 8 bits */
+	unsigned char ip_protocol;		/* TCP UDP - Protocol field: 8 bits */
+	unsigned short ip_checksum;		/* header checksum: 16 bits */
+	unsigned int ip_srcaddr;		/* source: 32 bits */
+	unsigned int ip_destaddr;		/* destination: 32 bits */
 
+};
 
+struct udp_header
+{
+	unsigned short source_port;		/* source port number */
+	unsigned short dest_port;		/* destination port */
+	unsigned short udp_length;		/* udp packet length */
+	unsigned short udp_checksum;		/* udp checksum (optional) */
+};
 
+struct tcp_header
+{
+	unsigned short source_port;		/* source port */
+	unsigned short dest_port;		/* destination port */
+	unsigned int sequence;			/* sequence number: 32 bits */
+	unsigned int acknowledge;		/* acknowledgement number: 32 bits */
 
+	unsigned char ns: 1;			/* nonce(?) sum flag (RFC 3540) */
+	unsigned char reserved_part1: 3;	/* according to the RFC */
+	unsigned char data_offset: 4;		/* the number of 32 bit words in the TCP header -
+						   indicates where the data begins - the length
+						   of the tcp header is always a multiple of 32 bits */
 
+	unsigned char fin: 1;			/* finish flag */
+	unsigned char syn: 1;			/* synchronize flag */
+	unsigned char rst: 1;			/* reset flag */
+	unsigned char psh: 1;			/* push flag */
+	unsigned char ack: 1;			/* acknowlegment flag */
+	unsigned char urg: 1;			/* urgent flag */
 
+	unsigned char ecn: 1;			/* ECN-echo flag */
+	unsigned char cwr: 1;			/* congestion window reduced flag */
 
+	unsigned short window;			/* window */
+	unsigned short checksum;		/* checksum */
+	unsigned short urgent_pointer;		/* urgent pointer */
+};
 
-
-
-
+struct icmp_header
+{
+	BYTE type;				/* ICMP error type */
+	BYTE code;				/* type sub code */
+	USHORT checksum;			/* checksum */
+	USHORT id;				/* id */
+	USHORT seq;				/* sequence */
+};
 
 #endif
